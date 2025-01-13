c8fb2d71a711d9758b4652584360120f437e15b4
Alexander-art-eng <154742965+Alexander-art-eng@users.noreply.github.com>
2025-01-13 14:07:50 +0200

    Fix the .venv in the project

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alexander\PycharmProjects\selinium-testing\.venv
+set VIRTUAL_ENV=D:\e2e_testing_frameworks\selinium_testing_learn\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
